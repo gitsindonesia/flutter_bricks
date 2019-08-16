644c43d1d99591b490bfc1497ddec246be0920ad
Ala Rai AbdiAllah <alaraiabdiallah@gmail.com>
2019-08-16 18:15:33 +0700

    RestAPI client helper

--- lib/globals/helpers/rest_api.dart
+++ lib/globals/helpers/rest_api.dart
@@ -2,8 +2,13 @@ import 'dart:convert';
 
 import 'package:flutter_bricks/globals/exception.dart';
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_with_interceptor.dart';
+
+import 'log_http_interceptor.dart';
 
 class RestAPI {
+
+  static client() => HttpWithInterceptor.build(interceptors: [LogHttpInterceptor()]);
 
   static responseJSON<T>(Response response, {onHttpOk}){
     switch(response.statusCode){
--- lib/providers/apis/meal_api.dart
+++ lib/providers/apis/meal_api.dart
@@ -1,16 +1,13 @@
 import 'package:flutter_bricks/globals/exception.dart';
-import 'package:flutter_bricks/globals/helpers/log_http_interceptor.dart';
 import 'package:flutter_bricks/globals/helpers/rest_api.dart';
 import 'package:flutter_bricks/models/meal/meal.dart';
-import 'package:http_interceptor/http_with_interceptor.dart';
 
 
 class MealAPIProvider{
 
   final baseUrl = "https://www.themealdb.com/api/json/v1/1/";
 
-  final http = HttpWithInterceptor.build(
-      interceptors: [LogHttpInterceptor()]);
+  final http = RestAPI.client();
 
   Future<List<Meal>> fetchMeals() async{
     final url = baseUrl + "latest.php";
